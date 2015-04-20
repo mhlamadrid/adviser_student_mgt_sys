7cd12aee334d397b3c7599d7141b765cefd8e4f8
jggarachico <jhggarachico@gmail.com>
2015-04-20 10:20:16 +0800

    fixed bugs in login/logout II

--- cmsc128_proj/assets/php/view_graphs.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	echo "<img src='img/_doge.png'>";
-	echo "o look a doge. Such graph. Much creative"
-?>
-
-	<img src="img/graph.png" height="400px" width="100%">
--- cmsc128_proj/assets/php/view_search.php
+++
@@ -1,7 +0,0 @@
-
-<h3 style="color: black;">
-<?php
-	echo "This is reserved for a filtered search."
-?>
-</h3>
-	<br><br><br><br><br><br><br>
